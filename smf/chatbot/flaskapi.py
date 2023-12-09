5131f7eaea00006fcff20fc5080c92f4e8b62219
sidinfinity <56204136+sidinfinity@users.noreply.github.com>
2023-12-09 15:29:18 -0800

    Update flaskapi & model

--- smf/chatbot/flaskapi.py
+++ smf/chatbot/flaskapi.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
 from llama_index import StorageContext, load_index_from_storage
+import os
+
+os.environ["OPENAI_API_KEY"] = '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'
 
 app = Flask(__name__)
-storage_context = StorageContext.from_defaults(persist_dir="<persist_dir>")
+storage_context = StorageContext.from_defaults(persist_dir="storage")
 index = load_index_from_storage(storage_context)
 query_engine = index.as_query_engine()
 
 def chatbot(input):
-    return query_engine.query(input)
+    return query_engine.query(input).response
 
 @app.route('/api', methods = ['GET'])
 def getresponse():
@@ -15,6 +18,7 @@ def getresponse():
     input = str(request.args['query'])
     answer = chatbot(input)
     d['output'] = answer
+    print(d)
     return d
     
 
